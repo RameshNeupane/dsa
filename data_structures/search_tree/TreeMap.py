1942639e98ff2a425cfae522c0296b8b39f53ad4
RameshNeupane <neupaneramesh555@gmail.com>
2024-10-02 10:28:01 +0545

    implement before and after methods in bst

--- data_structures/search_tree/TreeMap.py
+++ data_structures/search_tree/TreeMap.py
@@ -1,4 +1,4 @@
-from tree.LinkedBinaryTree import LinkedBinaryTree
+from data_structures.tree.LinkedBinaryTree import LinkedBinaryTree
 from map_and_hash.MapBase import MapBase, K, V
 
 type key = K
@@ -29,8 +29,61 @@ class TreeMap(LinkedBinaryTree, MapBase):
             if self.right(p) is not None:
                 return self._subtree_search(self.right(p), k)
         return p  # on unsuccessful search, p is position of last visited/search node
-    
 
-if __name__ == "__main__":
-    m = TreeMap()
-    print(m)
+    def _subtree_first_position(self, p: _Position) -> _Position:
+        """Return the first position in subtree rooted at p."""
+        cursor = p
+        while self.left(cursor) is not None:
+            cursor = self.left(cursor)
+        return cursor
+
+    def _subtree_last_position(self, p: _Position) -> _Position:
+        """Return the last position in subtree rooted at p."""
+        cursor = p
+        while self.right(cursor) is not None:
+            cursor = self.right(cursor)
+        return cursor
+
+    def first(self) -> _Position | None:
+        """Return the first position in the tree (or None if empty)."""
+        return self._subtree_first_position(self.root()) if len(self) > 0 else None
+
+    def last(self) -> _Position | None:
+        """Return the last position in the tree (or None if empty)."""
+        return self._subtree_last_position(self.root()) if len(self) > 0 else None
+
+    def before(self, p: _Position) -> _Position | None:
+        """Return the position just before p in the natural order.
+        Return None if p is the first position.
+        """
+        self._validate(p)
+        if self.left(p) is not None:
+            # last position in the left subtree of p
+            return self._subtree_last_position(self.left(p))
+        else:
+            cursor = p
+            parent = self.parent(cursor)
+            while parent is not None and cursor == self.left(parent):
+                cursor = parent
+                parent = self.parent(cursor)
+            return parent
+
+    def after(self, p: _Position) -> _Position | None:
+        """Return the position just after p in the natural order.
+        Return None if p is the last position."""
+        self._validate(p)
+        if self.right(p) is not None:
+            # first position in the right subtree of p
+            return self._subtree_first_position(self.right(p))
+        else:
+            cursor = p
+            parent = self.parent(cursor)
+            while parent is not None and cursor == self.right(parent):
+                cursor = parent
+                parent = self.parent(cursor)
+            return parent
+
+
+# if __name__ == "__main__":
+# m = TreeMap()
+# print(m)
